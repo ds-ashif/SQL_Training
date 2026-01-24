2b471ca182b35abb587a0d8caa25d9c79892ba4f
Ashif Ahamad <datascientist.ashif@gmail.com>
2026-01-24 12:02:20 +0530

    SQL procedures and its usage examples
    Added SQL procedures and comments for practice.

---
+++ Day2_StoredProcedures/Queries.sql
@@ -0,0 +1,47 @@
+--Action	Command
+--Create procedure	CREATE PROCEDURE
+--View procedure code	sp_helptext 'ProcName'
+--Run procedure	EXEC ProcName values
+--Separate batches	GO
+--Create Procedure MyPractice (used for creating a procedure)
+
+--First Procedure
+-- alter Procedure MyPractice
+-- @segmentName varchar(50),
+-- @city varchar(50)
+
+
+-- as
+-- Begin
+
+-- select * from Customers where City=@city or Segment=@segmentName;
+-- SELECT 'Hello SQL Procedures' AS Message;
+-- end--till  here is the syntax of a basic procedure in sql acts like a container
+-- go
+-- --Second Procedure
+-- --create procedure ProcedureSecond
+-- alter procedure ProcedureSecond
+-- as
+-- Begin
+-- 	Declare @total int;
+-- 	Select @total=COUNT(*) from Customers;
+-- 	if @total>2
+-- 		Print('More than 2');
+-- 	else
+-- 		Print('Less than 2');
+-- end
+
+--using go we can separate batches of sql code
+go 
+--sp_helptext is a system procedure.
+
+--It means:
+
+--�Show me the SQL code used to create this procedure�
+sp_helptext 'Procedurefirst'
+go
+Procedurefirst 'Retail','Chennai'
+go
+ProcedureSecond
+go 
+ProcedureThird 1,'2026-01-05'
