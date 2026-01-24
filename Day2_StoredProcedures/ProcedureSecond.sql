cfb4a9e045902473f6e6805055e577912e6b3231
Ashif Ahamad <datascientist.ashif@gmail.com>
2026-01-24 12:04:44 +0530

    Create ProcedureSecond to count customers
    This stored procedure checks the count of customers and prints a message based on the count.

---
+++ Day2_StoredProcedures/ProcedureSecond.sql
@@ -0,0 +1,19 @@
+﻿USE [Customer_Order]
+GO
+/****** Object:  StoredProcedurePractice [dbo].[ProcedureSecond]    Script Date: 22-1-26 11:39:24 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+--Second Procedure
+--create
+ALTER procedure [dbo].[ProcedureSecond]
+as
+Begin
+	Declare @total int;
+	Select @total=COUNT(*) from Customers;
+	if @total>2
+		Print('More than 2');
+	else
+		Print('Less than 2');
+end
