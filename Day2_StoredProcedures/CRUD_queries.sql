8784ed740b7c948e3986cce9e3bc674d1550836b
Ashif Ahamad <datascientist.ashif@gmail.com>
2026-01-24 12:13:13 +0530

    Create CRUD_queries.sql with basic SQL commands
    Added CRUD operations for basic data manipulation.

---
+++ Day2_StoredProcedures/CRUD_queries.sql
@@ -0,0 +1,21 @@
+﻿--CRUD stands for the four basic operations you can do on data:
+
+--Letter	Meaning	SQL command
+--C	Create	INSERT
+--R	Read	SELECT
+--U	Update	UPDATE
+--D	Delete	DELETE
+
+
+USE [dbo]
+
+
+
+SELECT  CustomerId, FullName
+FROM     Customers 
+UPDATE Customers
+SET  FullName = 'Ashif'
+WHERE  (CustomerId = 4)
+
+go  
+dbo 'SELECT'
