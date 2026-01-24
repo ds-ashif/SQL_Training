300df812764fa1f58ea5fd613905d482482a33ba
Ashif Ahamad <datascientist.ashif@gmail.com>
2026-01-24 12:06:39 +0530

    Create ProcedureThird stored procedure for orders

---
+++ Day2_StoredProcedures/ProcedureThird.sql
@@ -0,0 +1,21 @@
+﻿USE [Customer_Order]
+GO
+/****** Object:  StoredProcedurePractice [dbo].[ProcedureThird]    Script Date: 22-1-26 11:40:17 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+ALTER PROCEDURE [dbo].[ProcedureThird]
+	-- Add the parameters for the stored procedure here
+	@orderid int,
+	@orderDate date
+AS
+BEGIN
+	select *  from Orders where OrderId=@orderid or OrderDate=@orderDate;
+END
